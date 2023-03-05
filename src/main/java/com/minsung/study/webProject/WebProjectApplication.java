a3c8b7a67d3d0aad50ea46637714f25615a800cb
MinSungKim <timkim01@naver.com>
2023-03-05 22:33:23 +0900

    [Feat] first commit

---
+++ src/main/java/com/minsung/study/webProject/WebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.minsung.study.webProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebProjectApplication.class, args);
+	}
+
+}
