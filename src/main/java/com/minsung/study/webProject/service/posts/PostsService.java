f3a90956164ec715d1840657cb1284dd4a6cfe18
MinSungKim <timkim01@naver.com>
2023-03-20 22:14:07 +0900

    [Feat] 게시글 수정 기능

--- src/main/java/com/minsung/study/webProject/service/posts/PostsService.java
+++ src/main/java/com/minsung/study/webProject/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.minsung.study.webProject.service.posts;
 
+import com.minsung.study.webProject.domain.posts.Posts;
 import com.minsung.study.webProject.domain.posts.PostsRepository;
+import com.minsung.study.webProject.web.dto.PostsResponseDto;
 import com.minsung.study.webProject.web.dto.PostsSaveRequestDto;
+import com.minsung.study.webProject.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,5 +17,24 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id)
+        );
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id)
+        );
+
+        return new PostsResponseDto(entity);
     }
 }
--- src/main/java/com/minsung/study/webProject/web/PostApiController.java
+++ src/main/java/com/minsung/study/webProject/web/PostApiController.java
@@ -1,11 +1,11 @@
 package com.minsung.study.webProject.web;
 
 import com.minsung.study.webProject.service.posts.PostsService;
+import com.minsung.study.webProject.web.dto.PostsResponseDto;
 import com.minsung.study.webProject.web.dto.PostsSaveRequestDto;
+import com.minsung.study.webProject.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,5 +16,15 @@ public class PostApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
     }
 }
